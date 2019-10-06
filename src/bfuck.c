4973d955a2be794c9ae89bd14c09b6e8f204b3f8
uhl1k <jankuroman97@gmail.com>
2019-10-06 19:17:02 +0200

    Outline

--- src/bfuck.c
+++ src/bfuck.c
@@ -3,9 +3,27 @@
 #include <stdbool.h>
 #include <string.h>
 
+//  The array with program
+char prg[];
+//  The position in program
+int pos;
 
+void instruction (char inst)
+{
+
+}
+
+void cycle ()
+{
+
+}
+
+void interpret ()
+{
+
+}
 
 int main(int argc, char** argv)
 {
-    
+
 }
